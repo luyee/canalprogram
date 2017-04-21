a9703ee5eb033ad3eedbfa649d3e44eadd9c4736
yuqinggui <1012468686@qq.com>
2017-04-21 10:45:20 +0800

    添加部分hdfs内容

---
+++ src/main/java/com/gbicc/hdfs/HdfsMoveFile.java
@@ -0,0 +1,43 @@
+package com.gbicc.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.ResourceBundle;
+
+/**
+ * Created by root on 2017/4/21.
+ */
+public class HdfsMoveFile {
+    private static final Logger log = LoggerFactory.getLogger(HdfsMoveFile.class);
+
+    public static void main(String[] args) throws Exception {
+        ResourceBundle bundle = ResourceBundle.getBundle("movefile");
+        String targetDir = bundle.getString("targetDir");
+        String sourceDir = bundle.getString("sourceDir");
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        FileStatus[] statuses = fs.listStatus(new Path(sourceDir));
+        for (FileStatus status : statuses) {
+            if (status.isDirectory()) {
+                FileStatus[] fileStatuses = fs.listStatus(status.getPath());
+                for (FileStatus fileStatuse : fileStatuses) {
+                    String temp = fileStatuse.getPath().getName();
+                    String abspath = targetDir + File.separator + temp.split("_")[1];
+                    Path path = new Path(abspath);
+                    if (!fs.exists(path))
+                        fs.mkdirs(path);
+                    Path targetPath = new Path(path.toString() + File.separator + temp);
+                    fs.rename(fileStatuse.getPath(), targetPath);
+                    log.info("{}  =>>>   {}", fileStatuse.getPath(), path);
+                }
+            }
+        }
+        fs.close();
+    }
+}
--- src/test/java/com/gbicc/canal/Test5.java
+++ src/test/java/com/gbicc/canal/Test5.java
@@ -10,21 +10,28 @@ import java.util.Date;
  * Created by root on 2017/4/19.
  */
 public class Test5 {
+
     @Test
-    public void aa(){
-       Date date= DateUtils.stringToDate("2017-04-20 16:25:00",DateUtils.DATE_TO_STRING_DETAIAL_PATTERN);
-       System.out.println(date.getTime());
+    public void b2() {
+        String s = "e110_test1_20170419164828.txt.1492681491771.txt";
+        System.out.println(s.split(".txt")[0].split("_")[1]);
     }
 
     @Test
-    public void b1(){
-        System.out.println(DateUtils.DateToString(new Date(1492676700000L),DateUtils.DATE_TO_STRING_DETAIAL_PATTERN));
+    public void aa() {
+        Date date = DateUtils.stringToDate("2017-04-20 16:25:00", DateUtils.DATE_TO_STRING_DETAIAL_PATTERN);
+        System.out.println(date.getTime());
     }
 
     @Test
-    public void a1(){
-        String s="als.user_info,als.impower_to_user,als.customer_belong,als.customer_contacts,als.customer_special,als.customer_info,als.customer_relative,als.customer_partner,als.ent_info,als.ind_info,als.insure_loan_paymen_plan,als.insured_list,als.insure_special_agreement,als.insure_payment_plan,als.flow_record,als.insuretype,als.insure_message,als.business_type,als.business_typeset,als.policy_request,als.assetlist,als.guaranty_info,als.business_apply,als.business_contract,als.org_info,als.acct_payment_log,als.guaranty_contract,als.acct_trans_payment,als.loan_info,als.insuranceman_info,als.insured_info,als.project_info,als.cms_collateraltype_info,als.insure_info,als.acct_payment_schedule,als.acct_loan,als.acct_loan_change,als.acct_ahead,als.user_list,als.business_return,als.fund_strench_people,als.fee_infomation,als.customer_creditmmm\n";
+    public void b1() {
+        System.out.println(DateUtils.DateToString(new Date(1492676700000L), DateUtils.DATE_TO_STRING_DETAIAL_PATTERN));
+    }
+
+    @Test
+    public void a1() {
+        String s = "als.user_info,als.impower_to_user,als.customer_belong,als.customer_contacts,als.customer_special,als.customer_info,als.customer_relative,als.customer_partner,als.ent_info,als.ind_info,als.insure_loan_paymen_plan,als.insured_list,als.insure_special_agreement,als.insure_payment_plan,als.flow_record,als.insuretype,als.insure_message,als.business_type,als.business_typeset,als.policy_request,als.assetlist,als.guaranty_info,als.business_apply,als.business_contract,als.org_info,als.acct_payment_log,als.guaranty_contract,als.acct_trans_payment,als.loan_info,als.insuranceman_info,als.insured_info,als.project_info,als.cms_collateraltype_info,als.insure_info,als.acct_payment_schedule,als.acct_loan,als.acct_loan_change,als.acct_ahead,als.user_list,als.business_return,als.fund_strench_people,als.fee_infomation,als.customer_creditmmm\n";
         Arrays.asList(s.split(","))
-                .forEach(f-> System.out.println(f));
+                .forEach(f -> System.out.println(f));
     }
 }
